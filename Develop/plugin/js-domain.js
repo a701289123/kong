3c25d54ef882d43c83792182654352a9c2ec1bc6
jerry-lee <jerry-lee@webglsoft.com>
2023-12-11 14:49:30 +0800

    test

--- Develop/plugin/js-domain.js
+++ Develop/plugin/js-domain.js
@@ -1,6 +1,6 @@
 'use strict';
 
-class ServicecalloutPlugin {
+class DomainPlugin {
     constructor(config) {
         this.config = config;
     }
@@ -8,63 +8,171 @@ class ServicecalloutPlugin {
     async access(kong) {
         let path = await kong.request.getPathWithQuery();
         let host = await kong.request.getHost();
-        let api;
-        
-        try {                            
-            if (host == 'oocommapi2-dev.aswatson.com') {
-                if (path.includes("wtchk")) {
-                    api = "https://api.cmb8j9fjhz-apj3aswat1-s1-public.model-t.cc.commerce.ondemand.com/api/v2/wtchk";
-                } else if (path.includes("wtcmy")) {
-                    api = "https://api.cmb8j9fjhz-apj3aswat1-s1-public.model-t.cc.commerce.ondemand.com/api/v2/wtcmy";
-                } else if (path.includes("wtcsg")) {
-                    api = "https://api.cmb8j9fjhz-apj3aswat1-s1-public.model-t.cc.commerce.ondemand.com/api/v2/wtcsg";
-                } else if (path.includes("wtctw")) {
-                    api = "https://api.cmb8j9fjhz-apj4aswat1-s1-public.model-t.cc.commerce.ondemand.com/api/v2/wtctw";
-                } else if (path.includes("wtcph")) {
-                    api = "https://api.cmb8j9fjhz-apj4aswat1-s1-public.model-t.cc.commerce.ondemand.com/api/v2/wtcph";
-                } else if (path.includes("wtcid")) {
-                    api = "https://api.cmb8j9fjhz-aswatsona1-s1-public.model-t.cc.commerce.ondemand.com/api/v2/wtcid";
-                } else if (path.includes("wtcth")) {
-                    api = "https://api.cmb8j9fjhz-aswatsona1-s1-public.model-t.cc.commerce.ondemand.com/api/v2/wtcth";
-                } else if (path.includes("pnshk")) {
-                    api = "https://api.cmb8j9fjhz-apj2aswat1-s1-public.model-t.cc.commerce.ondemand.com/api/v2/pnshk";
-                } else if (path.includes("wtcvn")) {
-                    api = "https://api.cmb8j9fjhz-aswatsona1-s1-public.model-t.cc.commerce.ondemand.com/api/v2/wtcvn";
-                }
-            }
-            else if(host == 'oocommapi2.aswatson.com')
-            {
-                if (path.includes("wtchk")) {
-                    api = "https://api.watsons.com.hk/api/v2/wtchk";
-                } else if (path.includes("wtcmy")) {
-                    api = "https://api.watsons.com.my/api/v2/wtcmy";
-                } else if (path.includes("wtcsg")) {
-                    api = "https://api.watsons.com.sg/api/v2/wtcsg";
-                } else if (path.includes("wtctw")) {
-                    api = "https://api.watsons.com.tw/api/v2/wtctw";
-                } else if (path.includes("wtcph")) {
-                    api = "https://api.watsons.com.ph/api/v2/wtcph";
-                } else if (path.includes("wtcid")) {
-                    api = "https://api.watsons.co.id/api/v2/wtcid";
-                } else if (path.includes("wtcth")) {
-                    api = "https://api.watsons.co.th/api/v2/wtcth";
-                } else if (path.includes("pnshk")) {
-                    api = "https://api.parknshop.com/api/v2/pnshk";
-                } else if (path.includes("wtcvn")) {
-                    api = "https://api.watsons.vn/api/v2/wtcvn";
-                }
-            }
-            await kong.ctx.shared.set("domain",api)
-            
 
+        if (host == 'oocommapi2-dev.aswatson.com') {
+            await kong.ctx.shared.set("x_forwarded_host", "uat");
+        } else {
+            await kong.ctx.shared.set("x_forwarded_host", "prod");
+        }
+
+        const config = {
+            'wtchk': {
+                CURR: "HKD",
+                AI_KEY: 'GkEA8wSE1V3gPZjntiA5In4gvhU1xn3gwc1dhkNdRhfUAzFuoQ8ObA==',
+                BU: "wtchk",
+                AI_API: 'https://func-o2ai-wtchk-prod-eastasia.azurewebsites.net/api',
+                API_DEV: "https://api.cmb8j9fjhz-apj3aswat1-s1-public.model-t.cc.commerce.ondemand.com/api/v2/wtchk",
+                API_PROD: "https://api.watsons.com.hk/api/v2/wtchk",
+                ORIGIN_DEV: "https://wtchk.cmb8j9fjhz-apj3aswat1-s1-public.model-t.cc.commerce.ondemand.com",
+                ORIGIN_PROD: "https://www.watsons.com.hk",
+                UPSTREAM_DEV: "wtchk-dev",
+                UPSTREAM_PROD: "wtchk-prod"
+            },
+            'wtcmy': {
+                CURR: "MYR",
+                AI_KEY: 'MrfwaurIVrW-ft4cVyWFHO4leP3XGFRrjzaw6DZxyQk6AzFuRDzqFA==',
+                BU: "wtcmy",
+                AI_API: 'https://func-o2ai-wtcmy-prod-eastasia.azurewebsites.net/api',
+                API_DEV: "https://api.cmb8j9fjhz-apj3aswat1-s1-public.model-t.cc.commerce.ondemand.com/api/v2/wtcmy",
+                API_PROD: "https://api.watsons.com.my/api/v2/wtcmy",
+                ORIGIN_DEV: "https://wtcmy.cmb8j9fjhz-apj3aswat1-s1-public.model-t.cc.commerce.ondemand.com",
+                ORIGIN_PROD: "https://www.watsons.com.my",
+                UPSTREAM_DEV: "wtcmy-dev",
+                UPSTREAM_PROD: "wtcmy-prod"
+            },
+            'wtcsg': {
+                CURR: "SGD",
+                AI_KEY: 'Bsj5kBG6wMO2frEFp_14Ed38_MvqacqDBUU4InDSAG-6AzFuig0oEQ==',
+                BU: "wtcsg",
+                AI_API: 'https://func-o2ai-wtcsg-prod-eastasia.azurewebsites.net/api',
+                API_DEV: "https://api.cmb8j9fjhz-apj3aswat1-s1-public.model-t.cc.commerce.ondemand.com/api/v2/wtcsg",
+                API_PROD: "https://api.watsons.com.sg/api/v2/wtcsg",
+                ORIGIN_DEV: "https://wtcsg.cmb8j9fjhz-apj3aswat1-s1-public.model-t.cc.commerce.ondemand.com",
+                ORIGIN_PROD: "https://www.watsons.com.sg",
+                UPSTREAM_DEV: "wtcsg-dev",
+                UPSTREAM_PROD: "wtcsg-prod"
+            },
+            'wtctw': {
+                CURR: "TWD",
+                AI_KEY: 'T64BLJJpfcQp3E7QEzdIpWeXW4nvB25lclZkf5Ps883sAzFuy7YKrQ==',
+                BU: "wtctw",
+                AI_API: 'https://func-o2ai-wtctw-prod-eastasia.azurewebsites.net/api',
+                API_DEV: "https://api.cmb8j9fjhz-apj4aswat1-s1-public.model-t.cc.commerce.ondemand.com/api/v2/wtctw",
+                API_PROD: "https://api.watsons.com.tw/api/v2/wtctw",
+                ORIGIN_DEV: "https://wtctw.cmb8j9fjhz-apj4aswat1-s1-public.model-t.cc.commerce.ondemand.com",
+                ORIGIN_PROD: "https://www.watsons.com.tw",
+                UPSTREAM_DEV: "wtctw-dev",
+                UPSTREAM_PROD: "wtctw-prod"
+            },
+            'wtcph': {
+                CURR: "PHP",
+                AI_KEY: '2t8YayhZVVhyIGwe-hxOGKkEEyPB_gQOHOnuZno-m0AiAzFuukDYIg==',
+                BU: "wtcph",
+                AI_API: 'https://func-o2ai-wtcph-prod-eastasia.azurewebsites.net/api',
+                API_DEV: "https://api.cmb8j9fjhz-apj4aswat1-s1-public.model-t.cc.commerce.ondemand.com/api/v2/wtcph",
+                API_PROD: "https://api.watsons.com.ph/api/v2/wtcph",
+                ORIGIN_DEV: "https://wtcph.cmb8j9fjhz-apj4aswat1-s1-public.model-t.cc.commerce.ondemand.com",
+                ORIGIN_PROD: "https://www.watsons.com.ph",
+                UPSTREAM_DEV: "wtcph-dev",
+                UPSTREAM_PROD: "wtcph-prod"
+            },
+            'wtcid': {
+                CURR: "IDR",
+                AI_KEY: 'dT0LwyDbTv0aY2tFnGyeppHm52IZvLX-CIp8xQhZtqLDAzFu6rR87w==',
+                BU: "wtcid",
+                AI_API: 'https://func-o2ai-wtcid-prod-eastasia.azurewebsites.net/api',
+                API_DEV: "https://api.cmb8j9fjhz-aswatsona1-s1-public.model-t.cc.commerce.ondemand.com/api/v2/wtcid",
+                API_PROD: "https://api.watsons.co.id/api/v2/wtcid",
+                ORIGIN_DEV: "https://wtcid.cmb8j9fjhz-aswatsona1-s1-public.model-t.cc.commerce.ondemand.com",
+                ORIGIN_PROD: "https://www.watsons.co.id",
+                UPSTREAM_DEV: "wtcid-dev",
+                UPSTREAM_PROD: "wtcid-prod"
+            },
+            'wtcth': {
+                CURR: "THB",
+                AI_KEY: 'AhXXFyo3NJnl9EbV5RzmPgqBwYFQhZkRqbWDh5hhfuSfAzFuxtJpbQ==',
+                BU: "wtcth",
+                AI_API: 'https://func-o2ai-wtcth-prod-eastasia.azurewebsites.net/api',
+                API_DEV: "https://api.cmb8j9fjhz-aswatsona1-s1-public.model-t.cc.commerce.ondemand.com/api/v2/wtcth",
+                API_PROD: "https://api.watsons.co.th/api/v2/wtcth",
+                ORIGIN_DEV: "https://wtcth.cmb8j9fjhz-aswatsona1-s1-public.model-t.cc.commerce.ondemand.com",
+                ORIGIN_PROD: "https://www.watsons.co.th",
+                UPSTREAM_DEV: "wtcth-dev",
+                UPSTREAM_PROD: "wtcth-prod"
+            },
+            'pnshk': {
+                CURR: "HKD",
+                AI_KEY: 'BvBAOGpWfqMTpd2ldG0AatiktuFc-JuCxA5vL1KWygLSAzFuZTXrCA==',
+                BU: "pnshk",
+                AI_API: 'https://func-o2ai-pnshk-prod-eastasia.azurewebsites.net/api',
+                API_DEV: "https://api.cmb8j9fjhz-apj2aswat1-s1-public.model-t.cc.commerce.ondemand.com/api/v2/pnshk",
+                API_PROD: "https://api.pns.hk/api/v2/pnshk",
+                ORIGIN_DEV: "https://pnshk.cmb8j9fjhz-apj2aswat1-s1-public.model-t.cc.commerce.ondemand.com",
+                ORIGIN_PROD: "https://www.pns.hk",
+                UPSTREAM_DEV: "pnshk-dev",
+                UPSTREAM_PROD: "pnshk-prod"
+            },
+            'wtcvn': {
+                CURR: "VND",
+                AI_KEY: 'cwO5GdE6tgM7XbsJnYbadPdHo65iFTyxparEpl-Iv4hfAzFumwluYQ==',
+                BU: "wtcvn",
+                AI_API: 'https://func-o2ai-wtcvn-prod-eastasia.azurewebsites.net/api',
+                API_DEV: "https://api.cmb8j9fjhz-aswatsona1-s1-public.model-t.cc.commerce.ondemand.com/api/v2/wtcvn",
+                API_PROD: "https://api.watsons.vn/api/v2/wtcvn",
+                ORIGIN_DEV: "https://wtcvn.cmb8j9fjhz-aswatsona1-s1-public.model-t.cc.commerce.ondemand.com",
+                ORIGIN_PROD: "https://www.watsons.vn",
+                UPSTREAM_DEV: "wtcvn-dev",
+                UPSTREAM_PROD: "wtcvn-prod"
+            },
+            'wtctr': {
+                CURR: "TWD",
+                AI_KEY: 'cnxCT7KGPz3353r4lgYc4N4WMiK_s3cbOQn2UMG1KkFYAzFuljqEWw==',
+                BU: "wtctr",
+                AI_API: 'https://func-o2ai-wtctr-prod-eastasia.azurewebsites.net/api',
+                API_DEV: "https://api.cmb8j9fjhz-aswatsonh1-s1-public.model-t.cc.commerce.ondemand.com/api/v2/wtctr",
+                API_PROD: "https://api.watsons.com.tr/api/v2/wtctr",
+                ORIGIN_DEV: "https://wtctr.cmb8j9fjhz-aswatsonh1-s1-public.model-t.cc.commerce.ondemand.com",
+                ORIGIN_PROD: "https://www.watsons.com.tr",
+                UPSTREAM_DEV: "wtcrt-dev",
+                UPSTREAM_PROD: "wtcrt-prod"
+            },
+            'wwhk': {
+                CURR: "HKD",
+                AI_KEY: 'lE6kIwC2Cab0IEdgg-jsv_51aSBER6exeWB8pzpZCcGnAzFu70OAPw==',
+                BU: "wwhk",
+                AI_API: 'https://func-o2ai-wwhk-prod-eastasia.azurewebsites.net/api',
+                API_DEV: "",
+                API_PROD: "",
+                ORIGIN_DEV: "",
+                ORIGIN_PROD: "",
+                UPSTREAM_DEV: "wwhk-dev",
+                UPSTREAM_PROD: "wwhk-prod"
+            }
+        };
+
+        try {
+            for (let key in config) {
+                if (path.includes(key)) {
+                    let environment = host == 'oocommapi2-dev.aswatson.com' ? 'DEV' : 'PROD';
+                    await kong.ctx.shared.set("domain", config[key]['API_' + environment]);
+                    await kong.ctx.shared.set("baseSiteId", config[key].BU);
+                    await kong.ctx.shared.set("aiKey", config[key].AI_KEY);
+                    await kong.ctx.shared.set("curr", config[key].CURR);
+                    await kong.ctx.shared.set("upstream", config[key]['UPSTREAM_' + environment]);
+                    await kong.ctx.shared.set("url", config[key]['ORIGIN_' + environment]);
+                    await kong.ctx.shared.set("aiApi", config[key].AI_API);
+                }
+            }
         } catch (error) {
             await kong.response.setHeader('error', error.message)
         }
+
+
     }
 }
 
 module.exports = {
-    Plugin: ServicecalloutPlugin,    
+    Plugin: DomainPlugin,
     Version: '0.1.0',
-    Priority: 1001,
+    Priority: 1000002,
 };
--- Develop/plugin/js-servicecallout.js
+++ Develop/plugin/js-servicecallout.js
@@ -3,130 +3,151 @@
 const axios = require('axios');
 
 class ServicecalloutPlugin {
-  constructor(config) {
-    this.config = config;
-  }
-
-  async access(kong) {
-    const target = this.config.target;
-    const header = this.config.header;
-    const body = this.config.body;
-    const method = this.config.method;
-
-    var now = new Date();
-    now.setHours(now.getHours() + 8);
-    var year = now.getFullYear();
-    var month = ("0" + (now.getMonth() + 1)).slice(-2);
-    var day = ("0" + now.getDate()).slice(-2);
-    var hour = ("0" + now.getHours()).slice(-2);
-    var minute = ("0" + now.getMinutes()).slice(-2);
-    var second = ("0" + now.getSeconds()).slice(-2);
-    var formattedTime = year + month + day + hour + minute + second;
-
-    header  = header.reduce((acc, item) => {
-      const [key, value] = item.split(':');
-      acc[key] = value;
-      return acc;
-    }, {});
-
-
-    if (method == 'GET') {
-      try {
-        const response = await axios.get(target, { headers: header });
-        const responseData = response.data;
-        const setHeaderKong = Object.entries(responseData);
-        for (const [key, value] of setHeaderKong) {
-          await kong.service.request.setHeader(key.toString(), value.toString());
-        }
-      } catch (error) {
-        console.error(error);
-      }
+    constructor(config) {
+        this.config = config;
     }
-    else if (method == 'POST') {
-      try {
-        if (target.includes('subClub')) {                   //這邊if可以不用
-          body = {}
-          body.lastLoginTime = formattedTime
-          body.regTime = formattedTime
-          let hashedId = await kong.request.getHeader("X-JWT-HASH")
-          if(hashedId)
-          body.hashedId = hashedId
-          let host = await kong.request.getHost()
-          let path = await kong.request.getPath()
-          let PathSuffix = "/subClub/verify?fields=O2AI"
-          if (host == 'oocommapi2-dev.aswatson.com') {
-            if (path.includes("wtchk")) {
-                api = "https://api.cmb8j9fjhz-apj3aswat1-s1-public.model-t.cc.commerce.ondemand.com/api/v2/wtchk";
-            } else if (path.includes("wtcmy")) {
-                api = "https://api.cmb8j9fjhz-apj3aswat1-s1-public.model-t.cc.commerce.ondemand.com/api/v2/wtcmy";
-            } else if (path.includes("wtcsg")) {
-                api = "https://api.cmb8j9fjhz-apj3aswat1-s1-public.model-t.cc.commerce.ondemand.com/api/v2/wtcsg";
-            } else if (path.includes("wtctw")) {
-                api = "https://api.cmb8j9fjhz-apj4aswat1-s1-public.model-t.cc.commerce.ondemand.com/api/v2/wtctw";
-            } else if (path.includes("wtcph")) {
-                api = "https://api.cmb8j9fjhz-apj4aswat1-s1-public.model-t.cc.commerce.ondemand.com/api/v2/wtcph";
-            } else if (path.includes("wtcid")) {
-                api = "https://api.cmb8j9fjhz-aswatsona1-s1-public.model-t.cc.commerce.ondemand.com/api/v2/wtcid";
-            } else if (path.includes("wtcth")) {
-                api = "https://api.cmb8j9fjhz-aswatsona1-s1-public.model-t.cc.commerce.ondemand.com/api/v2/wtcth";
-            } else if (path.includes("pnshk")) {
-                api = "https://api.cmb8j9fjhz-apj2aswat1-s1-public.model-t.cc.commerce.ondemand.com/api/v2/pnshk";
+
+    async access(kong) {
+        let header = {};
+        let body;
+        let pathSuffix;
+        let queryParam = '';
+        let target;
+        const method = this.config.method.toLowerCase();
+        const contentType = await kong.request.getHeader('content-type');
+
+
+        const configHeader = this.config.header;
+        const ctxHeader = await kong.ctx.shared.get('ctxHeaderObj');
+         
+        if (configHeader){
+            const resultObject = {}; 
+            configHeader.forEach(item => {
+                const [key, value] = item.split(':');
+                resultObject[key] = value;
+            });  
+            header = resultObject;
+        }else if (ctxHeader){
+            header = ctxHeader;
+        }
+
+        
+        const configBody = this.config.body;
+        const requestBody = await kong.request.getBody();
+        
+        const ctxBody = 
+        await kong.ctx.shared.get('ctxBodyObj');
+
+        if (configBody){
+            const configBodyParsed = JSON.parse(configBody);
+            body = configBodyParsed;
+        }else if (ctxBody){
+            body = ctxBody;
+        }else {
+            body = requestBody;
+        }
+        
+        const configPathSuffix = this.config.pathSuffix;
+        const ctxPathSuffix =  await kong.ctx.shared.get('ctxPathSuffixStr');
+
+        const configQueryParam = this.config.queryParam;
+        const ctxQueryParam =  await kong.ctx.shared.get('ctxQueryParamStr');
+        
+        if (configQueryParam && configQueryParam.length !== 0){
+            queryParam = configQueryParam.map(item => {
+                const [key, value] = item.split(':');
+                return `${encodeURIComponent(key)}=${encodeURIComponent(value)}`;
+            }).join('&');
+        }else if (ctxQueryParam){
+            queryParam = ctxQueryParam;
+        }
+
+        if (queryParam){
+            if (configPathSuffix){
+                if (configPathSuffix.includes('?')) {
+                    pathSuffix = configPathSuffix + queryParam;
+                }
+                else {
+                    pathSuffix = configPathSuffix + '?' + queryParam;
+                }
+            }else if (ctxPathSuffix) {
+                if (configPathSuffix.includes('?')) {
+                    pathSuffix = ctxPathSuffix + queryParam;
+                }
+                else {
+                    pathSuffix = ctxPathSuffix + '?' + queryParam;
+                }
+            }    
+        }else {
+            if (configPathSuffix){
+                pathSuffix = configPathSuffix;
+            }else if (ctxPathSuffix) {
+                pathSuffix = ctxPathSuffix;
+            }   
+        }
+
+
+        const domain = await kong.ctx.shared.get('domain');
+        const targetEndpoint = this.config.targetEndpoint || await kong.ctx.shared.get('targetEndpoint');
+
+        if (pathSuffix && domain){
+            target = domain + pathSuffix;
+        }else if (targetEndpoint){
+            if (queryParam){
+                if (targetEndpoint.includes('?')) {
+                    target = targetEndpoint + queryParam;
+                }
+                else {
+                    target = targetEndpoint + '?' + queryParam;
+                }
+            }else {
+                target = targetEndpoint;
             }
-            target = api + PathSuffix
-          }
         }
-        await kong.service.request.setHeader('test', 'cicd2');
-        const requestBody = await kong.request.getBody()
-        const response = await axios.post(target, body, { headers: header });
-        // console.log(body)
-        const responseData = response.data;
-        const setHeaderKong = Object.entries(responseData);
-        for (const [key, value] of setHeaderKong) {
-          await kong.service.request.setHeader(key.toString(), value.toString());
-          if(key.toString() == 'memberNumber')
-          requestBody.memberNumber = value.toString()
+        
+
+        try {
+            let response;
+            if (method === 'get') {
+                response = await axios.get(target, { headers: header });
+            } else {
+                if (contentType.includes('json')){
+                    response = await axios[method](target, body, { headers: header });
+                }else {
+                    response = await axios[method](target, body, { headers: header, transformRequest: [function (data) {
+                        const formData = new URLSearchParams();
+                        for (const key in data) {
+                          formData.append(key, data[key]);
+                        }
+                        return formData.toString();
+                    }],});
+                }
+            }    
+
+            const responseData = response.data;
+            const setHeaderKong = Object.entries(responseData);
+            const obj = setHeaderKong.reduce((acc, [key, value]) => {
+                acc[key] = value;
+                return acc;
+            }, {});
+            await kong.ctx.shared.set('obj', obj);
+            
+        } catch (error) {
+            await kong.service.request.setHeader('axios${method}Error', error.message)
         }
-      } catch (error) {
-        // console.error(error);
-      }
     }
-    
-    // else if (method == 'PATCH') {
-    //   try {
-    //     const response = await axios.patch(target, body, { headers: header });
-    //     const responseData = response.data;
-    //     const setHeaderKong = Object.entries(responseData);
-    //     for (const [key, value] of setHeaderKong) {
-    //       await kong.service.request.setHeader(key.toString(), value.toString());
-    //     }
-    //   } catch (error) {
-    //     console.error(error);
-    //   }
-    // }
-    // else if (method == 'DELETE') {
-    //   try {
-    //     const response = await axios.delete(target, { headers: header });
-    //     const responseData = response.data;
-    //     const setHeaderKong = Object.entries(responseData);
-    //     for (const [key, value] of setHeaderKong) {
-    //       await kong.service.request.setHeader(key.toString(), value.toString());
-    //     }
-    //   } catch (error) {
-    //     console.error(error);
-    //   }
-    // }    
-  }
 }
 
 module.exports = {
   Plugin: ServicecalloutPlugin,
   Schema: [
-    { method: { type: "string" } },
-    { target: { type: "string" } },
-    // { params: { type: "string" } },
-    { header: { type: "array", elements: { type: "string" } } },
-        { body: { type: "array", elements: { type: "string" } } },
+      { method: { type: "string" } },
+      { header: { type: "array", elements: { type: "string" } } },
+      { body: { type: "string"}},
+      { pathSuffix: { type: "string" } },
+      { queryParam: { type: "array", elements: { type: "string" } } },
+      { targetEndpoint: { type: "string" } }
   ],
   Version: '0.1.0',
-  Priority: 0,
+  Priority: 1000,
 };
